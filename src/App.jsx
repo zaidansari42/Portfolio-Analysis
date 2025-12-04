48cbc53f6f17c09606664378cd38ff3e3e111641
Zaid Ansari <zaid@Zaids-Mac-mini.local>
2025-12-04 21:54:18 +0530

    REFACTOR: Seperate left side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import HomeIcon from "@mui/icons-material/Home";
-import ShowChartIcon from "@mui/icons-material/ShowChart";
-import {
-  AppBar,
-  Box,
-  Divider,
-  Drawer,
-  Grid,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Stack,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box } from "@mui/material";
 
 import HomePage from "./layout/pages/HomePage";
-import qodeLogo from "./assets/qodeadvisors_logo.jpeg";
 
 import "./App.css";
+import LeftSideBar from "./components/ui/LeftSideBar";
 
 function App() {
   const data = {
@@ -54,60 +38,7 @@ function App() {
             ml: `${drawerWidth}px`,
           }}
         ></AppBar>
-        <Drawer
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: drawerWidth,
-              boxSizing: "border-box",
-            },
-          }}
-          variant="permanent"
-          anchor="left"
-        >
-          <Toolbar>
-            <img
-              src={qodeLogo}
-              alt="Car"
-              style={{
-                maxWidth: "50px",
-                maxHeight: "70px",
-                objectFit: "cover",
-                marginRight: "10px",
-              }}
-            />
-            <Typography variant="h6" noWrap component="div">
-              Advisors LLP
-            </Typography>
-          </Toolbar>
-          <Divider />
-          <List>
-            {["Home", "Portfolio"].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <HomeIcon /> : <ShowChartIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {["About Me"].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <HomeIcon /> : <ShowChartIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
+        <LeftSideBar drawerWidth={drawerWidth}></LeftSideBar>
         <HomePage items={data.items}></HomePage>
       </Box>
     </>
---
+++ src/components/ui/LeftSideBar.jsx
@@ -0,0 +1,80 @@
+import HomeIcon from "@mui/icons-material/Home";
+import ShowChartIcon from "@mui/icons-material/ShowChart";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+import qodeLogo from "../../assets/qodeadvisors_logo.jpeg";
+
+const LeftSideBar = (props) => {
+  return (
+    <Drawer
+      sx={{
+        width: props.drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: props.drawerWidth,
+          boxSizing: "border-box",
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar>
+        <img
+          src={qodeLogo}
+          alt="Car"
+          style={{
+            maxWidth: "50px",
+            maxHeight: "70px",
+            objectFit: "cover",
+            marginRight: "10px",
+          }}
+        />
+        <Typography variant="h6" noWrap component="div">
+          Advisors LLP
+        </Typography>
+      </Toolbar>
+      <Divider />
+      <List>
+        {["Home", "Portfolio"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <HomeIcon /> : <ShowChartIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["About Me"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <HomeIcon /> : <ShowChartIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export default LeftSideBar;
