2b3a57f873ec7e248e8e26f22449ed3133b6edf5
Zaid Ansari <zaid@Zaids-Mac-mini.local>
2025-12-05 04:06:53 +0530

    Add date picker range

--- src/components/ui/CustomizedChart.jsx
+++ src/components/ui/CustomizedChart.jsx
@@ -1,8 +1,17 @@
 import { Box, Typography } from "@mui/material";
 import ReactECharts from "echarts-for-react";
 import { navData } from "../../data/navData";
+import { useState } from "react";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
 const CustomizedChart = () => {
+  // State Management
+  const [startDate, setStartDate] = useState(dayjs().subtract(5, "year"));
+  const [endDate, setEndDate] = useState(dayjs());
+
+  // Functions
   const formattedData = navData.map(([date, value]) => {
     const [dd, mm, yyyy] = date.split("-");
     return [`${yyyy}-${mm}-${dd}`, value.toFixed(2)];
@@ -14,6 +23,7 @@ const CustomizedChart = () => {
     return [date, Number((value - peak).toFixed(2))];
   });
 
+  // Input details for chart graph
   const option = {
     title: {
       text: "Quant Active Fund Gr",
@@ -39,8 +49,8 @@ const CustomizedChart = () => {
     dataZoom: [
       {
         type: "slider",
-        startValue: "2018-05-25",
-        endValue: "2024-01-24",
+        startValue: startDate.format("YYYY-MM-DD"),
+        endValue: endDate.format("YYYY-MM-DD"),
       },
       { type: "inside" },
     ],
@@ -77,15 +87,30 @@ const CustomizedChart = () => {
   };
 
   return (
-    <Box sx={{ marginTop: "30px" }}>
-      <Typography variant="h6" style={{ marginBottom: "10px" }}>
-        Equity Curve
-      </Typography>
-      <ReactECharts
-        style={{ height: "500px", width: "100%" }}
-        option={option}
-      />
-    </Box>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Box sx={{ marginTop: "30px" }}>
+        <Typography variant="h6" style={{ marginBottom: "10px" }}>
+          Equity Curve
+        </Typography>
+        <Box>
+          <DatePicker
+            label="Select Start Date"
+            value={startDate}
+            onChange={(newValue) => setStartDate(newValue)}
+          />
+          -
+          <DatePicker
+            label="Select End Date"
+            value={endDate}
+            onChange={(newValue) => setEndDate(newValue)}
+          />
+        </Box>
+        <ReactECharts
+          style={{ height: "500px", width: "100%" }}
+          option={option}
+        />
+      </Box>
+    </LocalizationProvider>
   );
 };
 
